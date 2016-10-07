06887e32fa0e88bd4841695cced031f3920b9da7
Jonathan Silal <jsilal@albany.edu>
2016-10-06 20:46:56 -0400

    constructor

--- Node.java
+++ Node.java
@@ -4,6 +4,14 @@ public class Node
 	private String data;
 	private Node next;
 	private Node previous;
+	// constructor of Data
+	   public Node(String Data)
+		{
+			data = Data;
+			next = null;
+			previous = null;
+		}
+
 }
 
 
