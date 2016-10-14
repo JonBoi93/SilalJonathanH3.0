00bcdba932454e4fc69a9c154dd535ef66b7f852
Jonathan Silal <jsilal@albany.edu>
2016-10-13 21:39:25 -0400

    Update Node

--- Node.java
+++ Node.java
@@ -5,56 +5,55 @@ public class Node
 	private Node next;
 	private Node previous;
 	// constructor of Data
-	   public Node(String Data)
+	public Node(String Data)
+	{
+		data = Data;
+		next = null;
+		previous = null;
+	}
+	// Constructor of data, previous, and next
+	public Node(String Data, Node Previous, Node Next)
+	{
+		data = Data;
+		previous = Previous;
+		next = Next;
+	}
+	// Accesssors
+	public String getData()
+	{
+		return data;
+	}
+	public Node getNext()
+	{
+		return next;
+	}
+	public Node getPrevious()
+	{
+	    return previous;
+	}
+	// Setters
+	public void setData(String Data)
 		{
 			data = Data;
-			next = null;
-			previous = null;
 		}
-// Constructor of data, previous, and next
-		public Node(String Data, Node Previous, Node Next)
+	public void setNext(Node Next)
+	{
+		next = Next;
+	}
+	public void setPrevious(Node Previous)
 		{
-			data = Data;
 			previous = Previous;
-			next = Next;
 		}
-// Accesssors
-		public String getData()
+		// Compare your data
+		public int Compare(String Data)
 		{
-			return data;
+			return Data.compareTo(data);
 		}
-		public Node getNext()
+		public int Compare(Node Compared)
 		{
-			return next;
+			return Compared.getData().compareTo(data);
 		}
-		public Node getPrevious()
-		{
-		    return previous;
-		}
-		// Setters
-		public void setData(String Data)
-			{
-				data = Data;
-			}
-		public void setNext(Node Next)
-		{
-			next = Next;
-		}
-		public void setPrevious(Node Previous)
-			{
-				previous = Previous;
-			}
-			// Compare your data
-			public int Compare(String Data)
-			{
-				return Data.compareTo(data);
-			}
-			public int Compare(Node Compared)
-			{
-				return Compared.getData().compareTo(data);
-			}
-
-}
+	}
 
 
 
